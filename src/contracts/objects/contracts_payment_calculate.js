f2506f32eb6afaae39b7ecb0e2e2abdbe0af84bb
chenxuan5858 <33675890+chenxuan5858@users.noreply.github.com>
2020-09-10 14:49:37 +0800

    update
    update

--- src/contracts/objects/contracts_payment_calculate.js
+++ src/contracts/objects/contracts_payment_calculate.js
@@ -54,14 +54,6 @@ async function contracts_payment_calculate(contractId)
   {servicefeestype = 'servicefee4'}
   };
 
-
- // let cstmoment = moment(cst);
-  //let cedmoment = moment(ced);
-  //let fpsmoment = moment(fps);
-  //let fpemoment = moment(fpe);
-
-  
-
   await receiptsObj.insert({
     fees_type: 'rentaldeposit',
     contract: contractId,
@@ -79,42 +71,88 @@ async function contracts_payment_calculate(contractId)
     amount: service_deposit,
     space: contract.space,
     });
-
+  
+  /*let i=0;
+  let contractMonth1 = moment(ced).add(1, 'd').diff(moment(cst), 'months', false); 
+  let circletimes1 = Math.ceil(contractMonth1/pc);
+  let initialst = i*pc;
+  let initialet = (i-1)*pc;
+  let ccstmoment = moment(ced).subtract(initialst, 'M').add(1, 'd').toDate();
+  let ccedmoment = moment(ced).subtract(initialet, 'M').toDate();
+  let judgementbefore = moment(ccstmoment).isBefore(cst);
+  let daysdifferent = moment(ccedmoment).add(1, 'd').diff(moment(cst), 'days');
+  let differentrantal = parseInt(daysdifferent*rs*srp);
+  let differentservice = parseInt(daysdifferent*rs*ssp);
+  */
+  
   if (is_ft == 'no')
   { 
-    let contractMonth1 = moment(ced).diff(moment(cst), 'months');
-    let circletimes1 = contractMonth1/pc;
-    for (let i=0;i<=circletimes1;i++)
-    {
-      let initialst = i*pc;
-      let initialet = (i+1)*pc;
-      let ccstmoment = moment(cst).add(initialst, 'M');
-      let ccedmoment = moment(cst).add(initialet, 'M').add(-.1, 'd');
-
-    await receiptsObj.insert({
-      fees_type: 'rentalfee',
-      contract: contractId,
-      fee_period_start: ccstmoment,
-      fee_period_end: ccedmoment,
-      amount: cycle_rental,
-      space: contract.space,
-      }); 
     
-    await receiptsObj.insert({
-      fees_type: servicefeestype,
-      contract: contractId,
-      fee_period_start: ccstmoment,
-      fee_period_end: ccedmoment,
-      amount: cycle_service,
-      space: contract.space,
-      }); 
+    let contractMonth1 = moment(ced).add(1, 'd').diff(moment(cst), 'months', false); 
+    let circletimes1 = Math.ceil(contractMonth1/pc);
+
+    for (let i=circletimes1;i>=1;i--)
+    {
+
+      let initialst = i*pc;
+      let initialet = (i-1)*pc;
+      let ccstmoment = moment(ced).subtract(initialst, 'M').add(1, 'd').toDate();
+      let ccedmoment = moment(ced).subtract(initialet, 'M').toDate();
+      let judgementbefore = moment(ccstmoment).isBefore(cst);
+      //let daysdifferent = moment(ccedmoment).add(1, 'd').diff(moment(cst), 'days');
+      //let differentrantal = parseInt(daysdifferent*rs*srp);
+      //let differentservice = parseInt(daysdifferent*rs*ssp);
+    if (judgementbefore = true)
+      { 
+        let daysdifferent = moment(ccedmoment).add(1, 'd').diff(moment(cst), 'days');
+        let differentrantal = parseInt(daysdifferent*rs*srp);
+        let differentservice = parseInt(daysdifferent*rs*ssp);
+        await receiptsObj.insert({
+          fees_type: 'rentalfee',
+          contract: contractId,
+          fee_period_start: cst,
+          fee_period_end: ccedmoment,
+          amount: differentrantal,
+          space: contract.space,
+          }); 
+        
+        await receiptsObj.insert({
+          fees_type: servicefeestype,
+          contract: contractId,
+          fee_period_start: cst,
+          fee_period_end: ccedmoment,
+          amount: differentservice,
+          space: contract.space,
+          }); 
+      }  
+      else if (judgementbefore = false)
+      {
+        await receiptsObj.insert({
+          fees_type: 'rentalfee',
+          contract: contractId,
+          fee_period_start: ccstmoment,
+          fee_period_end: ccedmoment,
+          amount: cycle_rental,
+          space: contract.space,
+          }); 
+    
+        await receiptsObj.insert({
+          fees_type: servicefeestype,
+          contract: contractId,
+          fee_period_start: ccstmoment,
+          fee_period_end: ccedmoment,
+          amount: cycle_service,
+          space: contract.space,
+          }); 
+      }
     
     }
-
-    
-    
+    //}
+  
 
   }
+
+  
  
   
 
